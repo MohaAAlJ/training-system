7bd7d599c2e4823fc1d0d966bd9f5977dcd5dd26
Moha A AL J <135424927+MohaAAlJ@users.noreply.github.com>
2025-12-07 14:50:36 +0200

    Add arabicFaker helper and minor code cleanup
    Introduced a new arabicFaker() helper in app/Helpers/General.php for generating Faker data with Arabic locale. Also removed unnecessary blank lines in AdministrativesFactory.php and MajorFactory.php.

---
+++ app/Helpers/General.php
@@ -0,0 +1,14 @@
+<?php
+
+use Faker\Factory;
+
+if (!function_exists('arabicFaker')) {
+    
+    /**
+     * Create a Faker instance with Arabic locale.
+     */
+    function arabicFaker()
+    {
+        return Factory::create('ar_SA');
+    }
+}
--- database/factories/MajorFactory.php
+++ database/factories/MajorFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories;
 
 use App\Models\Major;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Major>
  */
