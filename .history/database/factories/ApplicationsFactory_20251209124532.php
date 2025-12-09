6eada49f410d1789c27849a3e095687a3ded89cf
Adel Shurrab <143008607+Adel-Shurrab@users.noreply.github.com>
2025-12-09 12:48:53 +0200

    15/12/2025-12:48

--- .history/database/factories/ApplicationsFactory_20251209124532.php
+++ .history/database/factories/ApplicationsFactory_20251209124532.php
@@ -6,7 +6,6 @@ use App\Models\Applications;
 use App\Models\Departments;
 use App\Models\Trainees;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Applications>
