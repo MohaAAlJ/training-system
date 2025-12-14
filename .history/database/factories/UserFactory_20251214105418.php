928093582eed86fc36d6ec8b10dd3d3a6c4b35c3
Adel Shurrab <143008607+Adel-Shurrab@users.noreply.github.com>
2025-12-14 10:55:51 +0200

    ي

--- .history/database/factories/UserFactory_20251214105418.php
+++ .history/database/factories/UserFactory_20251214105418.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use App\Helpers\Constans;
-
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
@@ -23,10 +22,6 @@ class UserFactory extends Factory
             'status' => 'active',
             'role' => Constans::ROLE_MOH,
             'remember_token' => Str::random(10),
-        ];
-    }
-
-    public function admin(): static
     {
         return $this->state(fn(array $attributes) => [
             'role' => Constans::ROLE_ADMIN,
