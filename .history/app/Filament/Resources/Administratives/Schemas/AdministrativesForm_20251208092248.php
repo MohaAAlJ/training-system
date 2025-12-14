71924652144014cc2ab7f572a9840c355404d12b
Adel Shurrab <143008607+Adel-Shurrab@users.noreply.github.com>
2025-12-14 12:51:38 +0200

    administraitive

--- .history/app/Filament/Resources/Administratives/Schemas/AdministrativesForm_20251208092248.php
+++ .history/app/Filament/Resources/Administratives/Schemas/AdministrativesForm_20251208092248.php
@@ -5,9 +5,6 @@ namespace App\Filament\Resources\Administratives\Schemas;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
-use Filament\Forms\Components\Toggle;
-use Filament\Forms\Concerns\InteractsWithForms;
-use Filament\Forms\Contracts\HasForms;
 
 class AdministrativesForm
 {
@@ -16,29 +13,19 @@ class AdministrativesForm
         return $schema
             ->components([
                 TextInput::make('title')
-                    ->label('اسم الإدارة')
+                    ->label('اسم المديرية')
                     ->required()
                     ->maxLength(255),
                 TextInput::make('head_of_administrative')
-                    ->label('رئيس الإدارة')
+                    ->label('رئيس المديرية')
                     ->required()
                     ->maxLength(255),
-                Toggle::make('is_medical')
-                    ->label('هل هذه إدارة طبية؟')
-                    ->onColor('success')
-                    ->offColor('danger')
-                    ->live(),
                 Select::make('user_id')
                     ->label('المستخدم المسؤول')
                     ->relationship('user', 'name')
                     ->searchable()
                     ->preload()
                     ->required(),
-                Select::make('user_id')
-                    ->label('المدير الطبي')
-                    ->relationship('user', 'name')
-                    ->hidden(fn($get) => ! $get('is_medical')) 
-                    ->required(fn($get) => $get('is_medical')),
             ]);
     }
 }
