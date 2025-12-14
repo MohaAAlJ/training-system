84ea06ff192b4430b5f22ceb58b869c2efab0dbc
Adel Shurrab <143008607+Adel-Shurrab@users.noreply.github.com>
2025-12-14 11:34:55 +0200

    d

--- .history/app/Filament/Resources/Users/UsersResource_20251214113139.php
+++ .history/app/Filament/Resources/Users/UsersResource_20251214113139.php
@@ -9,7 +9,7 @@ use App\Filament\Resources\Users\Pages\ViewUsers;
 use App\Filament\Resources\Users\Schemas\UsersForm;
 use App\Filament\Resources\Users\Schemas\UsersInfolist;
 use App\Filament\Resources\Users\Tables\UsersTable;
-use App\Models\User;
+use App\Models\Users;
 use BackedEnum;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
