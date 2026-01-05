df6081ae73a9cd2e5c7a2ad793e5ef501936717c
Adel Shurrab <143008607+Adel-Shurrab@users.noreply.github.com>
2026-01-05 09:14:37 +0200

    Update

--- .history/config/pdf_20260105085625.php
+++ .history/config/pdf_20260105085625.php
@@ -4,7 +4,7 @@ return [
     'mode'                     => '',
     'format'                   => 'A4',
     'default_font_size'        => '12',
-    'default_font'             => 'notonaskh',
+    'default_font'             => 'sans-serif',
     'margin_left'              => 10,
     'margin_right'             => 10,
     'margin_top'               => 10,
