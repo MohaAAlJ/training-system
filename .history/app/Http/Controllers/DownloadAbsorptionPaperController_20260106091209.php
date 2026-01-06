874a3bced6b18c346b3e6614a14dc0002939e127
Adel Shurrab <143008607+Adel-Shurrab@users.noreply.github.com>
2026-01-06 09:27:29 +0200

    URL PDF Policies

--- .history/app/Http/Controllers/DownloadAbsorptionPaperController_20260106091209.php
+++ .history/app/Http/Controllers/DownloadAbsorptionPaperController_20260106091209.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Application;
 use Mccarlosen\LaravelMpdf\Facades\LaravelMpdf as Pdf;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DownloadAbsorptionPaperController extends Controller
 {
