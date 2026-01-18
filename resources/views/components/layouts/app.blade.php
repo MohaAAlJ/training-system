062e1b8ffaa3dbd588c645486ae6005338ee99ce
Adel Shurrab <143008607+Adel-Shurrab@users.noreply.github.com>
2026-01-18 12:32:20 +0200

    k

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="rtl">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,10 +22,12 @@
     <!-- Livewire Styles -->
     @livewireStyles
 </head>
+
 <body style="margin: 0; padding: 0;">
     {{ $slot }}
 
     <!-- Livewire Scripts -->
     @livewireScripts
 </body>
+
 </html>
