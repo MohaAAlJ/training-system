5467be26aa8018de35192659d25397f4a0fb088c
Moha A AL J <135424927+MohaAAlJ@users.noreply.github.com>
2026-01-21 10:06:02 +0200

    Update BackupExport.php

--- app/Exports/BackupExport.php
+++ app/Exports/BackupExport.php
@@ -26,10 +26,10 @@ class BackupExport
 
         $latestFile = collect($files)->sort()->last();
 
-        if (!$latestFile) {
-            // This aborts nicely instead of crashing with TypeError
-            abort(404, 'No backup file was created. Please check logs.');
-        }
+        // if (!$latestFile) {
+        //     // This aborts nicely instead of crashing with TypeError
+        //     abort(404, 'No backup file was created. Please check logs.');
+        // }
 
         // 3. Return the file for download
         return response()->download($backupDisk->path($latestFile));
